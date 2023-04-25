2377835a69eb9ce6210a1e143f30e04dbffbae3a
kahyunKo <bufar1014@gmail.com>
2023-04-25 23:54:20 +0900

    Car Class added

---
+++ src/practice04/gitTest01.java
@@ -0,0 +1,15 @@
+package practice04;
+
+class Car {
+	String name = "sonata";
+	String color = "red";
+}
+
+public class gitTest01 {
+	public static void main(String[] args) {
+		Car car1 = new Car();
+		
+		System.out.println(car1.name);
+		System.out.println(car1.color);
+	}
+}
